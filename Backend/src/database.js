a701aa30daee36db3e87a19bc575953b94918cb4
Brayan Leon <bragim1721@gmail.com>
2025-05-12 07:40:51 -0500

    fix: agregar variable MYSQLPORT en la conexión

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -7,7 +7,8 @@ const connection = mysql.createConnection({
     host:process.env.DB_HOST,
     database:process.env.DB_DATABASE,
     user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD
+    password:process.env.DB_PASSWORD,
+    port: process.env.MYSQLPORT || 3306
 });
 
 const getConnection = async ()=> await connection;
